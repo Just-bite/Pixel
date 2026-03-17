3c7d6ae8d6079fa7470e33cdbd1c551092bb5283
Penumbra <konstantinfedenov@gmail.com>
2026-03-17 23:41:01 +0300

    technical debt shiffed and acknowledged

--- Pixel/contextpannel.h
+++ Pixel/contextpannel.h
@@ -43,8 +43,6 @@ private:
     void addLabeledWidget(QHBoxLayout* layout, const QString& text, QWidget* widget);
 
     QLabel* m_lbl_placeholder;
-
-    // ИСПРАВЛЕНИЕ: m_layer_group теперь QWidget*, а не QGroupBox*
     QGroupBox *m_geometry_group, *m_style_group;
     QWidget *m_layer_group;
 
